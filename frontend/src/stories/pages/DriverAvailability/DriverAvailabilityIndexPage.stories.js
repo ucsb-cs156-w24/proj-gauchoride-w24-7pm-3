ae0765dc631f90faebbf9510ffba85590dbb2550
Jason Gu <1057337859@qq.com>
2024-03-05 17:11:00 -0800

    complete driver availability index page and tests

---
+++ frontend/src/stories/pages/DriverAvailability/DriverAvailabilityIndexPage.stories.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import DriverAvailabilityIndexPage from 'main/pages/DriverAvailability/DriverAvailabilityIndexPage';
+
+export default {
+    title: 'pages/DriverAvailability/DriverAvailabilityIndexPage',
+    component: DriverAvailabilityIndexPage
+};
+
+const Template = () => <DriverAvailabilityIndexPage />;
+
+export const Default = Template.bind({});
+
+
+
+
